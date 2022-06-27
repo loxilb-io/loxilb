f222201afc703bef238b8397a25642b244ad234d
inho <inhogog2@netlox.io>
2022-06-27 18:00:57 +0900

    Add function using Load Balance API

--- api/restapi/handler/common.go
+++ api/restapi/handler/common.go
@@ -1,10 +1,13 @@
 package handler
 
 import (
+	cmn "loxilb/common"
 	"net/http"
 
 	"github.com/go-openapi/runtime"
 )
+
+var ApiHooks cmn.NetHookInterface
 
 type ResultResponse struct {
 	Result string `json:"result"`
--- api/restapi/handler/loadbalancer.go
+++ api/restapi/handler/loadbalancer.go
@@ -1,37 +1,39 @@
 package handler
 
 import (
-	"fmt"
 	"loxilb/api/restapi/operations"
+	cmn "loxilb/common"
 
 	"github.com/go-openapi/runtime/middleware"
 )
 
 func ConfigPostLoadbalancer(params operations.PostConfigLoadbalancerParams) middleware.Responder {
-	//return middleware.NotImplemented("operation operations.PostConfigLoadbalancer has not yet been implemented")
+	var lbServ cmn.LbServiceArg
+	var lbEps []cmn.LbEndPointArg
+	var lbRules cmn.LbRuleMod
 
-	/*
-		lbServ := LbServiceArg{"10.10.10.1", 2020, "tcp"}
-		lbEps := []LbEndPointArg{
-			{
-			"32.32.32.1",
-			5001,
-			1,
-			},
-			{
-			"32.32.32.1",
-			5001,
-			2,
-			},
-		}
+	lbServ.ServIP = params.Attr.ExternalIPAddress
+	lbServ.ServPort = uint16(params.Attr.Port)
+	lbServ.Proto = params.Attr.Protocol
 
-		mh.zr.Rules.AddNatLbRule(lbServ, lbEps[:])
-	*/
-	fmt.Printf("params.Attr: %v\n", params.Attr)
-	for _, data := range params.Attr.EndPoint {
-		fmt.Printf("data.EndpointIPAddress: %v\n", data.EndpointIPAddress)
-		fmt.Printf("data: %v\n", data)
+	for _, data := range params.Attr.Endpoints {
+		lbEps = append(lbEps, cmn.LbEndPointArg{
+			EpIP:   data.EndpointIPAddress,
+			EpPort: uint16(data.TargetPort),
+			Weight: uint8(data.Weight),
+		})
 	}
 
-	return &ResultResponse{Result: "Hi"}
+	lbRules.Serv = lbServ
+	lbRules.Eps = append(lbRules.Eps, lbEps...)
+
+	//fmt.Printf("lbEps: %v\n", lbEps)
+	//fmt.Printf("lbServ: %v\n", lbServ)
+	//fmt.Printf("lbRules: %v\n", lbRules)
+
+	_, err := ApiHooks.NetLbRuleAdd(&lbRules)
+	if err != nil {
+		return &ResultResponse{Result: err.Error()}
+	}
+	return &ResultResponse{Result: "Success"}
 }
