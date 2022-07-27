4b72cfcd5ce6c5a8291ad672e6a04cd070783245
Nikhil Malik <nikhil.malik@netlox.io>
2022-07-27 16:14:35 +0900

    ping.sh fixed for L2

--- ebpf/utils/ping.sh
+++ ebpf/utils/ping.sh
@@ -22,6 +22,7 @@ for ns1 in `sudo ip netns | cut -d " " -f 1`; do
 			continue ;
  		fi;
 
+		echo "********************************************************************"
         #Reset onlyL2 flag
         if [ \( $ns1L2 -eq 0 \) -a \( $onlyL2 -eq 1 \) ]
         then
@@ -37,6 +38,8 @@ for ns1 in `sudo ip netns | cut -d " " -f 1`; do
             onlyL2=1
             #echo "$ns2 can do only L2 with $ns1"
         fi
+		echo "$ns1 -> $ns2"
+
         if [ $onlyL2 == 1 ]
         then
             net2=( `sudo ip netns exec $ns2 ip route | grep src | cut -d " " -f 1` )
@@ -57,42 +60,66 @@ for ns1 in `sudo ip netns | cut -d " " -f 1`; do
                     fi
                 done
             done
+		    for h1 in "${!hosts1[@]}"
+		    do
+			    echo "(${hosts1[h1]}) -> (${hosts2[h1]})";
+                #echo "CMD : sudo ip netns exec $ns1 ping $h2 -f -c 500 -I $h1"
+				sudo ip netns exec $ns1 ping ${hosts2[h1]} -f -c 500  2>&1> /dev/null;
+				if [[ $? -eq 0 ]]
+				then
+				    echo "Ping [OK]"
+				else
+				    echo "Ping [NOK]"
+                    code=1
+				fi
+                if [ ! -z "$gw1" ]
+                then
+				    echo "(${hosts1[h1]}) -> ($gw1)";
+				    sudo ip netns exec $ns1 ping $gw1 -f -c 500 2>&1> /dev/null;
+                    if [[ $? -eq 0 ]]
+				    then
+					    echo "Ping [OK]"
+				    else
+					    echo "Ping [NOK]"
+                        code=1
+				    fi
+
+                fi
+		    done
 
         else    
 		    dev2=`sudo ip netns exec $ns2 ip route | grep "default" | cut -d " " -f 5`
 		    hosts2=( `sudo ip netns exec $ns2 ip addr show dev $dev2 | grep -w inet | cut -d " " -f 6 | cut -d "/" -f 1` )
-        fi
-		echo "********************************************************************"
-		echo "$ns1 -> $ns2"
-		for h1 in "${hosts1[@]}"
-		do
-			for h2 in "${hosts2[@]}"
-			do
-				echo "($h1) -> ($h2)";
-                #echo "CMD : sudo ip netns exec $ns1 ping $h2 -f -c 500 -I $h1"
-				sudo ip netns exec $ns1 ping $h2 -f -c 500 -I $h1 2>&1> /dev/null;
-				if [[ $? -eq 0 ]]
-				then
-					echo "Ping [OK]"
-				else
-					echo "Ping [NOK]"
-                    code=1
-				fi
-			done
-            if [ ! -z "$gw1" ]
-            then
-				echo "($h1) -> ($gw1)";
-				sudo ip netns exec $ns1 ping $gw1 -f -c 500 2>&1> /dev/null;
-                if [[ $? -eq 0 ]]
-				then
-					echo "Ping [OK]"
-				else
-					echo "Ping [NOK]"
-                    code=1
-				fi
+		    for h1 in "${hosts1[@]}"
+		    do
+			    for h2 in "${hosts2[@]}"
+			    do
+				    echo "($h1) -> ($h2)";
+                    #echo "CMD : sudo ip netns exec $ns1 ping $h2 -f -c 500 -I $h1"
+				    sudo ip netns exec $ns1 ping $h2 -f -c 500 -I $h1 2>&1> /dev/null;
+				    if [[ $? -eq 0 ]]
+				    then
+					    echo "Ping [OK]"
+				    else
+					    echo "Ping [NOK]"
+                        code=1
+				    fi
+			    done
+                if [ ! -z "$gw1" ]
+                then
+				    echo "($h1) -> ($gw1)";
+				    sudo ip netns exec $ns1 ping $gw1 -f -c 500 2>&1> /dev/null;
+                    if [[ $? -eq 0 ]]
+				    then
+					    echo "Ping [OK]"
+				    else
+					    echo "Ping [NOK]"
+                        code=1
+				    fi
 
-            fi
-		done
+                fi
+		    done
+        fi
 	done
 done
 echo "********************************************************************"
