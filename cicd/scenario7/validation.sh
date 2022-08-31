f64de3d3786ca89a8453772067fc56573266450a
Nikhil Malik <nikhil.malik@netlox.io>
2022-08-31 18:20:40 +0900

    sleep added in validation script

--- cicd/scenario7/validation.sh
+++ cicd/scenario7/validation.sh
@@ -8,7 +8,7 @@ $hexec l3ep2 socat udp-listen:8080,fork exec:'echo server2' &
 $hexec l3ep3 socat udp-listen:8080,fork exec:'echo server3' &
 
 $dexec llb1 loxicmd create lb 20.20.20.1 --udp=2020:8080 --endpoints=31.31.31.1:1,32.32.32.1:1,33.33.33.1:1
-sleep 4
+sleep 10
 code=0
 servArr=( "server1" "server2" "server3" )
 ep=( "31.31.31.1" "32.32.32.1" "33.33.33.1" )
