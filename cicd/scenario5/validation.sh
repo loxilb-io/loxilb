255189525e113e5f78d16c19bf24496c02bd82f2
Nikhil Malik <nikhil.malik@netlox.io>
2022-09-01 10:23:34 +0900

    sleep increased in validation script

--- cicd/scenario5/validation.sh
+++ cicd/scenario5/validation.sh
@@ -6,7 +6,7 @@ $hexec l3ep2 node ./server2.js &
 $hexec l3ep3 node ./server3.js &
 
 $dexec llb1 loxicmd create lb 20.20.20.1 --tcp=2020:8080 --endpoints=31.31.31.1:1,32.32.32.1:1,33.33.33.1:1
-sleep 20
+sleep 30
 code=0
 servArr=( "server1" "server2" "server3" ) 
 for i in {1..5}
