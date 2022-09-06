cf22cada0880df5260b46e3905f9d9af2793bb42
Nikhil Malik <nikhil.malik@netlox.io>
2022-09-06 16:18:14 +0900

    curl detailed output

--- cicd/scenario3/validation.sh
+++ cicd/scenario3/validation.sh
@@ -38,8 +38,8 @@ do
 for j in {0..2}
 do
     #$hexec ue$k ping ${ep[j]} -f -c 5 -W 1;
-    #res=$($hexec ue$k curl -s 88.88.88.88:2020)
-    res=$($hexec ue$k curl 88.88.88.8:2020)
+    res=$($hexec ue$k curl 88.88.88.88:2020)
+    #res=$($hexec ue$k curl 88.88.88.8:2020)
     echo -e $res
     if [[ $res != "${servArr[j]}" ]]
     then
