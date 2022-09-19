5129c3231ebc6f08513b5645f9d31d90592003ef
Nikhil Malik <nikhil.malik@netlox.io>
2022-09-19 15:44:27 +0900

    More cases for Tunnel over SCTP loadbalancing added

---
+++ cicd/scenario10/validation.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+source ../common.sh
+echo SCENARIO-10
+$hexec l3e1 ./server server1 &
+$hexec l3e2 ./server server2 &
+$hexec l3e3 ./server server3 &
+
+sleep 5
+code=0
+servArr=( "server1" "server2" "server3" )
+ep=( "25.25.25.1" "26.26.26.1" "27.27.27.1" )
+j=0
+waitCount=0
+while [ $j -le 2 ]
+do
+    #res=$($hexec ue1 curl ${ep[j]}:8080)
+    res=`$hexec h1 ./client ${ep[j]} 8080`
+    #echo $res
+    if [[ $res == "${servArr[j]}" ]]
+    then
+        echo "$res UP"
+        j=$(( $j + 1 ))
+    else
+        echo "Waiting for ${servArr[j]}(${ep[j]})"
+        waitCount=$(( $waitCount + 1 ))
+        if [[ $waitCount == 10 ]];
+        then
+            echo "All Servers are not UP"
+            echo SCENARIO-9 [FAILED]
+            exit 1
+        fi
+
+    fi
+    sleep 1
+done
+
+for k in {1..2}
+do
+for i in {1..2}
+do
+for j in {0..2}
+do
+    res=$($hexec h$k ./client 88.88.88.88 2020)
+    echo -e $res
+    if [[ $res != "${servArr[j]}" ]]
+    then
+        echo -e "Expected ${servArr[j]}, Received : $res"
+        if [[ "$res" != *"server"* ]];
+        then
+            echo "llb1 ct"
+            $dexec llb1 loxicmd get ct
+            echo "llb2 ct"
+            $dexec llb2 loxicmd get ct
+            echo "llb2 ip neigh"
+            $dexec llb2 ip neigh
+        fi
+        code=1
+    fi
+    sleep 1
+done
+done
+done
+if [[ $code == 0 ]]
+then
+    echo SCENARIO-10 [OK]
+else
+    echo SCENARIO-10 [FAILED]
+fi
+exit $code
+
---
+++ cicd/scenario9/config.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+source ../common.sh
+
+echo "#########################################"
+echo "Spawning all hosts"
+echo "#########################################"
+
+spawn_docker_host loxilb llb1
+spawn_docker_host loxilb llb2
+spawn_docker_host host ue1
+spawn_docker_host host ue2
+spawn_docker_host host l3e1
+spawn_docker_host host l3e2
+spawn_docker_host host l3e3
+
+echo "#########################################"
+echo "Connecting and configuring  hosts"
+echo "#########################################"
+
+
+connect_docker_hosts ue1 llb1
+connect_docker_hosts ue2 llb1
+connect_docker_hosts llb1 llb2
+
+config_docker_host --host1 ue1 --host2 llb1 --ptype phy --addr 32.32.32.1/24 --gw 32.32.32.254
+config_docker_host --host1 llb1 --host2 ue1 --ptype phy --addr 32.32.32.254/24
+config_docker_host --host1 ue2 --host2 llb1 --ptype phy --addr 31.31.31.1/24 --gw 31.31.31.254
+config_docker_host --host1 llb1 --host2 ue2 --ptype phy --addr 31.31.31.254/24
+config_docker_host --host1 llb1 --host2 llb2 --ptype phy --addr 10.10.10.59/24
+config_docker_host --host1 llb2 --host2 llb1 --ptype phy --addr 10.10.10.56/24
+
+connect_docker_hosts l3e1 llb2
+connect_docker_hosts l3e2 llb2
+connect_docker_hosts l3e3 llb2
+
+config_docker_host --host1 l3e1 --host2 llb2 --ptype phy --addr 25.25.25.1/24 --gw 25.25.25.254
+config_docker_host --host1 llb2 --host2 l3e1 --ptype phy --addr 25.25.25.254/24
+config_docker_host --host1 l3e2 --host2 llb2 --ptype phy --addr 26.26.26.1/24 --gw 26.26.26.254
+config_docker_host --host1 llb2 --host2 l3e2 --ptype phy --addr 26.26.26.254/24
+config_docker_host --host1 l3e3 --host2 llb2 --ptype phy --addr 27.27.27.1/24 --gw 27.27.27.254
+config_docker_host --host1 llb2 --host2 l3e3 --ptype phy --addr 27.27.27.254/24
+
+$hexec llb1 ip route add 25.25.25.0/24 via 10.10.10.56 dev ellb1llb2
+$hexec llb1 ip route add 26.26.26.0/24 via 10.10.10.56 dev ellb1llb2
+$hexec llb1 ip route add 27.27.27.0/24 via 10.10.10.56 dev ellb1llb2
+$hexec llb1 ip route add 88.88.88.8/32 via 10.10.10.56 dev ellb1llb2
+
+$hexec llb2 ip route add 31.31.31.0/24 via 10.10.10.59 dev ellb2llb1
+$hexec llb2 ip route add 32.32.32.0/24 via 10.10.10.59 dev ellb2llb1
+
+##llb1 config
+#ue1
+$dexec llb1 loxicmd create session user1 88.88.88.88 --accessNetworkTunnel 1:10.10.10.56 --coreNetworkTunnel=1:10.10.10.59
+ 
+$dexec llb1 loxicmd create sessionulcl user1 --ulclArgs=11:32.32.32.1
+ 
+#ue2
+$dexec llb1 loxicmd create session user2 88.88.88.88 --accessNetworkTunnel 2:10.10.10.56 --coreNetworkTunnel=2:10.10.10.59
+ 
+$dexec llb1 loxicmd create sessionulcl user2 --ulclArgs=12:31.31.31.1
+
+##llb2 config
+#ue1
+$dexec llb2 loxicmd create session user1 32.32.32.1 --accessNetworkTunnel 1:10.10.10.59 --coreNetworkTunnel=1:10.10.10.56
+ 
+
+$dexec llb2 loxicmd create sessionulcl user1 --ulclArgs=11:88.88.88.88
+
+#ue2
+$dexec llb2 loxicmd create session user2 31.31.31.1 --accessNetworkTunnel 2:10.10.10.59 --coreNetworkTunnel=2:10.10.10.56
+ 
+$dexec llb2 loxicmd create sessionulcl user2 --ulclArgs=12:88.88.88.88
+
+
+##Create LB rule
+$dexec llb2 loxicmd create lb 88.88.88.88 --sctp=2020:8080 --endpoints=25.25.25.1:1,26.26.26.1:1,27.27.27.1:1
