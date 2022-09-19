ff36a06e51d6398f50882036c204e32887bf49f8
Nikhil Malik <nikhil.malik@netlox.io>
2022-09-19 15:43:27 +0900

    socat replaced with go server for UDP scenario

---
+++ cicd/scenario7/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+        "fmt"
+        "net"
+        "os"
+)
+
+func main() {
+        arguments := os.Args
+        if len(arguments) == 1 {
+                fmt.Println("Please provide a valid port!")
+                return
+        }
+        ADDR := ":"+arguments[1]
+
+        s, err := net.ResolveUDPAddr("udp4", ADDR)
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
+
+        connection, err := net.ListenUDP("udp4", s)
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
+
+        defer connection.Close()
+        buffer := make([]byte, 1024)
+        count := 0
+        for {
+                if count == 5 {
+                    return
+                }
+
+                _, addr, err := connection.ReadFromUDP(buffer)
+                //fmt.Print("-> ", string(buffer[0:n-1]))
+                count++
+                data := []byte(arguments[2])
+                //fmt.Printf("data: %s\n", string(data))
+                _, err = connection.WriteToUDP(data, addr)
+                if err != nil {
+                        fmt.Println(err)
+                        return
+                }
+        }
+}
