a731dd68fa10cf3fa8fba3c7d4937ac206e87a18
Dip J <dj.nlx@netlox.io>
2022-11-25 16:05:53 +0900

    fw support in native ebpf continued

--- loxinet/dpbroker.go
+++ loxinet/dpbroker.go
@@ -191,6 +191,34 @@ type PolDpWorkQ struct {
 	Status *DpStatusT
 }
 
+// FwOpT - type of firewall operation
+type FwOpT uint8
+
+// Fw type constants
+const (
+	DpFwDrop FwOpT = iota + 1
+	DpFwFwd
+	DpFwRdr
+)
+
+// FwDpWorkQ - work queue entry for fw related operation
+type FwDpWorkQ struct {
+	Work      DpWorkT
+	Status    *DpStatusT
+	ZoneNum   int
+	SrcIP 	  net.IPNet
+	DstIP 	  net.IPNet
+	L4SrcMin  uint16
+	L4SrcMax  uint16
+	L4DstMin  uint16
+	L4DstMax  uint16
+	Proto     uint8
+	HwMark    int
+	FwType    FwOpT
+	FwVal1	  uint32
+	FwVal2	  uint32
+}
+
 // NatT - type of NAT
 type NatT uint8
 
