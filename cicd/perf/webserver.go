95b25836e1a61550eb72cb48a0e652e7dd5ce811
dj-nlx <dj.nlx@netlox.io>
2022-12-20 23:22:33 +0900

    Some perf measurement scripts

---
+++ cicd/perf/webserver.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+        "log"
+        "net/http"
+)
+
+func main() {
+        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+        })
+        if err := http.ListenAndServe(":8080", nil); err != nil {
+                log.Fatal("ListenAndServe: ", err)
+        }
+}
