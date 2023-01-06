de87b6803b12da583a3c830fb9066797dd9a3290
dj-nlx <dj.nlx@netlox.io>
2023-01-06 12:51:11 +0900

    CICD for kubernetes via k3s

---
+++ cicd/k3s/config.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+source ../common.sh
+
+echo "#########################################"
+echo "Spawning all hosts"
+echo "#########################################"
+
+spawn_docker_host --dock-type loxilb --dock-name llb1 --with-bgp yes --bgp-config $(pwd)/llb1_gobgp_config --with-ka in --ka-config $(pwd)/keepalived_config
+spawn_docker_host --dock-type loxilb --dock-name llb2 --with-bgp yes --bgp-config $(pwd)/llb2_gobgp_config --with-ka in --ka-config $(pwd)/keepalived_config
+spawn_docker_host --dock-type host --dock-name ep1
+spawn_docker_host --dock-type host --dock-name ep2
+spawn_docker_host --dock-type host --dock-name ep3
+spawn_docker_host --dock-type host --dock-name r1
+spawn_docker_host --dock-type host --dock-name r2 --with-bgp yes --bgp-config $(pwd)/quagga_config
+spawn_docker_host --dock-type host --dock-name user
+
+echo "#########################################"
+echo "Connecting and configuring  hosts"
+echo "#########################################"
+
+connect_docker_hosts user r1
+connect_docker_hosts r1 llb1
+connect_docker_hosts r1 llb2
+connect_docker_hosts llb1 r2
+connect_docker_hosts llb2 r2
+connect_docker_hosts r2 ep1
+connect_docker_hosts r2 ep2
+connect_docker_hosts r2 ep3
+
+## Make network for k3s connectivity
+sudo ip link add ellb1sys type veth peer name esysllb1
+sleep 3
+sudo ip link set ellb1sys netns llb1
+sleep 3
+sudo ip -n llb1 link set ellb1sys up
+sudo ip -n llb1 addr add 12.12.12.1/24 dev ellb1sys
+sudo ip link set esysllb1 up
+sudo ip addr add 12.12.12.254/24 dev esysllb1
+
+sudo ip link add ellb2sys type veth peer name esysllb2
+sleep 3
+sudo ip link set ellb2sys netns llb2
+sleep 3
+sudo ip -n llb2 link set ellb2sys up
+sudo ip -n llb2 addr add 14.14.14.1/24 dev ellb2sys
+sudo ip link set esysllb2 up
+sudo ip addr add 14.14.14.254/24 dev esysllb2
+
+#node1 config
+config_docker_host --host1 user --host2 r1 --ptype phy --addr 1.1.1.1/24 --gw 1.1.1.254
+config_docker_host --host1 r1 --host2 user --ptype phy --addr 1.1.1.254/24
+
+create_docker_host_vlan --host1 r1 --host2 llb1 --id 11 --ptype untagged
+create_docker_host_vlan --host1 llb1 --host2 r1 --id 11 --ptype untagged
+
+config_docker_host --host1 r1 --host2 llb1 --ptype vlan --id 11 --addr 11.11.11.254/24 --gw 11.11.11.11
+config_docker_host --host1 llb1 --host2 r1 --ptype vlan --id 11 --addr 11.11.11.1/24
+
+create_docker_host_vlan --host1 r1 --host2 llb2 --id 11 --ptype untagged
+create_docker_host_vlan --host1 llb2 --host2 r1 --id 11 --ptype untagged
+config_docker_host --host1 llb2 --host2 r1 --ptype vlan --id 11 --addr 11.11.11.2/24
+
+
+create_docker_host_vlan --host1 llb1 --host2 r2 --id 10 --ptype untagged
+config_docker_host --host1 llb1 --host2 r2 --ptype vlan --id 10 --addr 10.10.10.1/24
+create_docker_host_vlan --host1 llb2 --host2 r2 --id 10 --ptype untagged
+config_docker_host --host1 llb2 --host2 r2 --ptype vlan --id 10 --addr 10.10.10.2/24
+
+create_docker_host_vlan --host1 r2 --host2 llb1 --id 10 --ptype untagged
+create_docker_host_vlan --host1 r2 --host2 llb2 --id 10 --ptype untagged
+create_docker_host_vlan --host1 r2 --host2 ep1 --id 10 --ptype untagged
+create_docker_host_vlan --host1 r2 --host2 ep2 --id 10 --ptype untagged
+create_docker_host_vlan --host1 r2 --host2 ep3 --id 10 --ptype untagged
+config_docker_host --host1 r2 --host2 llb1 --ptype vlan --id 10 --addr 10.10.10.254/24
+
+create_docker_host_vlan --host1 r2 --host2 ep1 --id 31 --ptype untagged
+config_docker_host --host1 r2 --host2 ep1 --ptype vlan --id 31 --addr 31.31.31.254/24
+
+create_docker_host_vlan --host1 ep1 --host2 r2 --id 31 --ptype untagged
+config_docker_host --host1 ep1 --host2 r2 --ptype vlan --id 31 --addr 31.31.31.1/24 --gw 31.31.31.254
+
+create_docker_host_vlan --host1 r2 --host2 ep2 --id 32 --ptype untagged
+config_docker_host --host1 r2 --host2 ep2 --ptype vlan --id 32 --addr 32.32.32.254/24
+
+create_docker_host_vlan --host1 ep2 --host2 r2 --id 32 --ptype untagged
+config_docker_host --host1 ep2 --host2 r2 --ptype vlan --id 32 --addr 32.32.32.1/24 --gw 32.32.32.254
+
+create_docker_host_vlan --host1 r2 --host2 ep3 --id 33 --ptype untagged
+config_docker_host --host1 r2 --host2 ep3 --ptype vlan --id 33 --addr 33.33.33.254/24
+
+create_docker_host_vlan --host1 ep3 --host2 r2 --id 33 --ptype untagged
+config_docker_host --host1 ep3 --host2 r2 --ptype vlan --id 33 --addr 33.33.33.1/24 --gw 33.33.33.254
+
+##Pod networks
+$hexec r1 ip route add 20.20.20.1/32 via 11.11.11.11
+add_route llb1 1.1.1.0/24 11.11.11.254
+add_route llb2 1.1.1.0/24 11.11.11.254
+
+sleep 1
+##Create LB rule
+create_lb_rule llb1 20.20.20.1 --tcp=2020:8080 --endpoints=31.31.31.1:1,32.32.32.1:1,33.33.33.1:1 --mode=fullnat --bgp
+create_lb_rule llb2 20.20.20.1 --tcp=2020:8080 --endpoints=31.31.31.1:1,32.32.32.1:1,33.33.33.1:1 --mode=fullnat --bgp
+
+# keepalive will take few seconds to be UP and running with valid states
+sleep 60
+
+# K3s setup
+
+# If k3s setup exists, remove it
+if [[ -f "/usr/local/bin/k3s-uninstall.sh" ]]; then
+  /usr/local/bin/k3s-uninstall.sh
+fi
+
+# Install k3s without external cloud-manager and disabled servicelb
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable traefik --disable servicelb --disable-cloud-controller --kubelet-arg cloud-provider=external
+"   sh -
+
+sleep 10
+
+# Check kubectl works
+sudo kubectl get pods
+
+# Install Bird to work with k3s
+sudo apt install bird2 --yes
+
+sudo cp -f bird_config/bird.conf /etc/bird/bird.conf
+sudo systemctl restart bird
+
+# Remove taints in k3s if any (usually happens if started without cloud-manager) 
+sudo kubectl taint nodes --all node.cloudprovider.kubernetes.io/uninitialized=false:NoSchedule-
+
+sleep 5
+
+# Start loxi-ccm as k3s daemonset
+sudo kubectl apply -f loxi-ccm.yaml
+
+# Start nginx pods and services for test
+sudo kubectl apply -f nginx.yml
+sudo kubectl apply -f nginx-svc-lb.yml
+
+sleep 5 
+
+# External LB service must be created by now
+sudo kubectl get svc
---
+++ cicd/k3s/rmconfig.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "#########################################"
+echo "Removing testbed"
+echo "#########################################"
+
+source ../common.sh
+
+disconnect_docker_hosts user r1
+disconnect_docker_hosts r1 llb1
+disconnect_docker_hosts r1 llb2
+disconnect_docker_hosts llb1 r2
+disconnect_docker_hosts llb2 r2
+disconnect_docker_hosts r2 ep1
+disconnect_docker_hosts r2 ep2
+disconnect_docker_hosts r2 ep3
+
+delete_docker_host ka_llb1
+delete_docker_host ka_llb2
+delete_docker_host llb1
+delete_docker_host llb2
+delete_docker_host user
+delete_docker_host r1
+delete_docker_host r2
+delete_docker_host ep1
+delete_docker_host ep2
+delete_docker_host ep3
+sudo ip link del esysllb1
+sudo ip link del esysllb2
+
+sudo kubectl delete -f nginx-svc-lb.yml >> /dev/null 2>&1
+sudo kubectl delete -f nginx.yml >> /dev/null 2>&1
+sudo kubectl delete -f loxi-ccm.yaml >> /dev/null 2>&1
+
+# If k3s setup exists, remove it
+if [[ -f "/usr/local/bin/k3s-uninstall.sh" ]]; then
+  /usr/local/bin/k3s-uninstall.sh
+fi
+
+sudo apt-get remove bird2 --yes
+
+echo "#########################################"
+echo "Removed testbed"
+echo "#########################################"
