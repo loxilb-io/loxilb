3c2ea0e251549aec1734f6b3ef79ab5a7042dc4d
loxi-admin <admin@netlox.io>
2023-01-09 14:39:56 +0900

    Updated options to listen to all IP for API server

--- options/options.go
+++ options/options.go
@@ -10,7 +10,7 @@ var Opts struct {
 	Version           bool           `short:"v" long:"version" description:"Show loxilb version"`
 	NoApi             bool           `short:"a" long:"api" description:"Run Rest API server"`
 	NoNlp             bool           `short:"n" long:"nonlp" description:"Do not register with nlp"`
-	Host              string         `long:"host" description:"the IP to listen on" default:"localhost" env:"HOST"`
+	Host              string         `long:"host" description:"the IP to listen on" default:"0.0.0.0" env:"HOST"`
 	Port              int            `long:"port" description:"the port to listen on for insecure connections" default:"11111" env:"PORT"`
 	TLSHost           string         `long:"tls-host" description:"the IP to listen on for tls, when not specified it's the same as --host" env:"TLS_HOST"`
 	TLSPort           int            `long:"tls-port" description:"the port to listen on for secure connections" default:"8091" env:"TLS_PORT"`
