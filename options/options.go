2252e81b3bb3d035b41e0eeb88662d265e380c10
dj-nlx <dj.nlx@netlox.io>
2023-01-19 22:58:00 +0900

    Annonation for self description

--- options/options.go
+++ options/options.go
@@ -17,5 +17,6 @@ var Opts struct {
 	TLSCertificate    flags.Filename `long:"tls-certificate" description:"the certificate to use for secure connections" default:"/opt/loxilb/cert/server.crt" env:"TLS_CERTIFICATE"`
 	TLSCertificateKey flags.Filename `long:"tls-key" description:"the private key to use for secure connections" default:"/opt/loxilb/cert/server.key" env:"TLS_PRIVATE_KEY"`
 	ClusterNodes      string         `long:"cluster" description:"Comma-separated list of cluter-node IP Addresses" default:"none"`
+	ClusterSelf       int            `long:"self" description:"annonation of self in cluster" default:"0"`
 	LogLevel          string         `long:"loglevel" description:"One of debug,info,error,warning,notice,critical,emergency,alert" default:"debug"`
 }
