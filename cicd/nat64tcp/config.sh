edf2320e85ec0899f7c0ebc5dfbc93be1ca53c8d
Dip J <dj.nlx@netlox.io>
2023-01-26 15:18:15 +0900

    Tweaks to sanity CI for nat64

--- cicd/nat64tcp/config.sh
+++ cicd/nat64tcp/config.sh
@@ -65,5 +65,5 @@ $hexec llb1 ip -6 addr add 4ffe::2/64 dev ellb1l3ep1
 $hexec llb1 ip -6 addr add 5ffe::2/64 dev ellb1l3ep2
 $hexec llb1 ip -6 addr add 6ffe::2/64 dev ellb1l3ep3
 
-sleep 5
+sleep 10
 $dexec llb1 loxicmd create lb 2001::1 --tcp=2020:8080 --endpoints=31.31.31.1:1,32.32.32.1:1,33.33.33.1:1
