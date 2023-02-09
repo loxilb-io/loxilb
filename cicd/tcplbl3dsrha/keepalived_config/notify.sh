ff93611183700cb05b4831742aca547040bc0d7d
Nikhil Malik <nikhil.malik@netlox.io>
2023-02-09 16:34:47 +0900

    removed unwanted files

--- cicd/tcplbl3dsrha/keepalived_config/notify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo $1 $2 is in $3 state > /root/keepalive.state
-curl -X 'POST' 'http://0.0.0.0:11111/netlox/v1/config/cistate' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ "instance": "'$2'", "state" : "'$3'" }'
--- cicd/tcplbl3dsrha/try.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-declare -A ip
-ip["llb1"]="10.10.10.1"
-ip["llb2"]="10.10.10.1"
-echo ${ip["llb1"]}
