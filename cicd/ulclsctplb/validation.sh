3a4aebe94706296c8abb7097b90c74944c50403a
Nikhil Malik <nikhil.malik@netlox.io>
2023-04-13 16:40:02 +0900

    ulclsctplb test validation updated using socat

--- cicd/ulclsctplb/validation.sh
+++ cicd/ulclsctplb/validation.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 source ../common.sh
 echo SCENARIO-ulclsctplb
-$hexec l3e1 ./server server1 &
-$hexec l3e2 ./server server2 &
-$hexec l3e3 ./server server3 &
+$hexec l3e1 socat -v -T0.5 sctp-l:8080,bind=25.25.25.1,reuseaddr,fork system:"echo 'server1'; cat" >/dev/null 2>&1 &
+$hexec l3e2 socat -v -T0.5 sctp-l:8080,bind=26.26.26.1,reuseaddr,fork system:"echo 'server2'; cat" >/dev/null 2>&1 &
+$hexec l3e3 socat -v -T0.5 sctp-l:8080,bind=27.27.27.1,reuseaddr,fork system:"echo 'server3'; cat" >/dev/null 2>&1 &
 
 sleep 5
 code=0
 servArr=( "server1" "server2" "server3" )
 ep=( "25.25.25.1" "26.26.26.1" "27.27.27.1" )
+ueIp=( "" "32.32.32.1" "31.31.31.1" )
 j=0
 waitCount=0
 while [ $j -le 2 ]
 do
     #res=$($hexec ue1 curl ${ep[j]}:8080)
-    res=`$hexec ue1 ./client ${ep[j]} 8080`
+    res=`$hexec ue1 socat -T10 - SCTP:${ep[j]}:8080,bind=${ueIp[1]}`
     #echo $res
     if [[ $res == "${servArr[j]}" ]]
     then
@@ -27,6 +28,7 @@ do
         then
             echo "All Servers are not UP"
             echo SCENARIO-ulclsctplb [FAILED]
+            sudo pkill -9 socat 2>&1 > /dev/null
             exit 1
         fi
 
@@ -40,7 +42,8 @@ for i in {1..2}
 do
 for j in {0..2}
 do
-    res=$($hexec ue$k ./client 88.88.88.88 2020)
+    #res=$($hexec ue$k ../common/sctp_client ${ueIp[k]} 88.88.88.88 2020)
+    res=$($hexec ue$k socat -T10 - SCTP:88.88.88.88:2020,bind=${ueIp[k]})
     echo -e $res
     if [[ $res != "${servArr[j]}" ]]
     then
@@ -66,5 +69,5 @@ then
 else
     echo SCENARIO-ulclsctplb [FAILED]
 fi
+sudo pkill -9 socat 2>&1 > /dev/null
 exit $code
-
