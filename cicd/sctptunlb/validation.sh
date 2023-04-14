53e04e00df3b9958a5357bb4963415017cc77349
Nikhil Malik <nikhil.malik@netlox.io>
2023-04-14 12:08:05 +0900

    Typo corrected in the script

--- cicd/sctptunlb/validation.sh
+++ cicd/sctptunlb/validation.sh
@@ -43,20 +43,20 @@ for i in {1..2}
 do
 for j in {0..2}
 do
-    res=$($hexec h$k ../common/sctp_client ${ueIp[k]} 88.88.88.88 2020)
+    res=$($hexec h$k ../common/sctp_client ${ueIP[k]} 88.88.88.88 2020)
     echo -e $res
     if [[ $res != "${servArr[j]}" ]]
     then
         echo -e "Expected ${servArr[j]}, Received : $res"
-        if [[ "$res" != *"server"* ]];
-        then
-            echo "llb1 ct"
-            $dexec llb1 loxicmd get ct
-            echo "llb2 ct"
-            $dexec llb2 loxicmd get ct
-            echo "llb2 ip neigh"
-            $dexec llb2 ip neigh
-        fi
+#        if [[ "$res" != *"server"* ]];
+#        then
+#            echo "llb1 ct"
+#            $dexec llb1 loxicmd get ct
+#            echo "llb2 ct"
+#            $dexec llb2 loxicmd get ct
+#            echo "llb2 ip neigh"
+#            $dexec llb2 ip neigh
+#        fi
         code=1
     fi
     sleep 1
