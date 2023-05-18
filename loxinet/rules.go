eecdce892db1a9b4ab0d41de1aa8d174ddd37db7
Trekkie Coder <trekkie@netlox.io>
2023-05-18 16:45:15 +0900

    gh-265 Fixes for fast liveness check on ep creation
    (cherry picked from commit c9debd3f16263f6494ef1b23ffe1fc76c532dd7f)

--- loxinet/rules.go
+++ loxinet/rules.go
@@ -902,7 +902,7 @@ func (R *RuleH) syncEPHostState2Rule(rule *ruleEnt, checkNow bool) bool {
 			}
 
 			for idx, n := range na.endPoints {
-				sOk := R.IsEPHostActive(makeEPKey(n.xIP.String(), sType, n.xPort))
+				sOk := R.IsEpHostActive(makeEPKey(n.xIP.String(), sType, n.xPort))
 				np := &na.endPoints[idx]
 				if sOk == false {
 					if np.noService == false {
@@ -1127,6 +1127,10 @@ func (R *RuleH) AddNatLbRule(serv cmn.LbServiceArg, servSecIPs []cmn.LbSecIpArg,
 	r.CI = cmn.CIDefault
 
 	R.modNatEpHost(r, natActs.endPoints, true)
+
+	if r.ActChk {
+		R.syncEPHostState2Rule(r, true)
+	}
 
 	tk.LogIt(tk.LogDebug, "nat lb-rule added - %d:%s-%s\n", r.ruleNum, r.tuples.String(), r.act.String())
 
@@ -1548,6 +1552,9 @@ func (R *RuleH) AddEPHost(apiCall bool, hostName string, name string, args epHos
 	R.lepHID++
 
 	R.epMap[epKey] = ep
+
+	// Liveness check upfront
+	R.epCheckNow(ep)
 
 	tk.LogIt(tk.LogDebug, "ep-host added %v:%d\n", epKey, ep.hID)
 
