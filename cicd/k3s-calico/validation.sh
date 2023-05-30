666811229e589f216454f138feccffcf6a4e06fc
Trekkie Coder <trekkie@netlox.io>
2023-05-30 23:45:28 +0900

    Added cicd for k3s with calico

---
+++ cicd/k3s-calico/validation.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+source ../common.sh
+echo calico-k3s
+
+if [ "$1" ]; then
+  KUBECONFIG="$1"
+fi
+
+# Set space as the delimiter
+IFS=' '
+
+for((i=0; i<120; i++))
+do
+  extLB=$(sudo kubectl $KUBECONFIG get svc | grep "nginx-lb")
+  read -a strarr <<< "$extLB"
+  len=${#strarr[*]}
+  if [[ $((len)) -lt 6 ]]; then
+    echo "Can't find nginx-lb service"
+    sleep 1
+    continue
+  fi 
+  if [[ ${strarr[3]} != *"none"* ]]; then
+    extIP=${strarr[3]}
+    break
+  fi
+  echo "No external LB allocated"
+  sleep 1
+done
+
+## Any routing updates  ??
+sleep 30
+
+echo $extIP
+out=$($hexec user curl -s --connect-timeout 10 http://$extIP:80) 
+
+if [[ ${out} == *"Welcome to nginx"* ]]; then
+  echo calico-k3s [OK]
+else
+  echo calico-k3s [FAILED]
+  ## Dump some debug info
+  echo "llb1 lb-info"
+  $dexec llb1 loxicmd get lb
+  echo "llb1 route-info"
+  $dexec llb1 ip route
+  echo "llb2 lb-info"
+  $dexec llb2 loxicmd get lb
+  echo "llb2 route-info"
+  $dexec llb2 ip route
+  echo "r1 route-info"
+  $dexec r1 ip route
+  exit 1
+fi
+
+out=$($hexec user curl -s --connect-timeout 10 http://$extIP:55002) 
+
+if [[ ${out} == *"Welcome to nginx"* ]]; then
+  echo "calico-k3s (kube-loxilb) [OK]"
+else
+  echo "calico-k3s (kube-loxilb) [FAILED]"
+  ## Dump some debug info
+  echo "llb1 lb-info"
+  $dexec llb1 loxicmd get lb
+  echo "llb1 route-info"
+  $dexec llb1 ip route
+  echo "llb2 lb-info"
+  $dexec llb2 loxicmd get lb
+  echo "llb2 route-info"
+  $dexec llb2 ip route
+  echo "r1 route-info"
+  $dexec r1 ip route
+  exit 1
+fi
+
+out=$($hexec user ../common/udp_client $extIP 55003)
+if [[ ${out} == *"Client"* ]]; then
+  echo calico-k3s udp [OK]
+else
+  echo calico-k3s udp [FAILED]
+fi
+exit
