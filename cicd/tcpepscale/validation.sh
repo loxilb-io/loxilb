c6b0bbd5922290a9ec7d9d97957bf26fcea70d46
Trekkie Coder <trekkie@netlox.io>
2023-06-01 11:55:22 +0900

    Print more things on epscale ci failure

--- cicd/tcpepscale/validation.sh
+++ cicd/tcpepscale/validation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source ../common.sh
 echo SCENARIO-tcpepscale
-$hexec l3ep1 socat -v -T0.05 tcp-l:8080,reuseaddr,fork system:"echo 'server1'; cat" >/dev/null 2>&1 &
-$hexec l3ep2 socat -v -T0.05 tcp-l:8080,reuseaddr,fork system:"echo 'server2'; cat" >/dev/null 2>&1 &
-$hexec l3ep3 socat -v -T0.05 tcp-l:8080,reuseaddr,fork system:"echo 'server3'; cat" >/dev/null 2>&1 &
+$hexec l3ep1 socat -v -T5 tcp-l:8080,reuseaddr,fork system:"echo 'server1'; cat" >/dev/null 2>&1 &
+$hexec l3ep2 socat -v -T5 tcp-l:8080,reuseaddr,fork system:"echo 'server2'; cat" >/dev/null 2>&1 &
+$hexec l3ep3 socat -v -T5 tcp-l:8080,reuseaddr,fork system:"echo 'server3'; cat" >/dev/null 2>&1 &
 
 sleep 140
 echo "Inactive endpoints"
@@ -41,6 +41,7 @@ do
     if [[ $res != "server1" ]]
     then
         code=1
+        ps -ef |grep socat
     fi
     sleep 1
 done
