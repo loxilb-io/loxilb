58585d7c13149e9e504fa07122336538de5208a0
Trekkie Coder <trekkie@netlox.io>
2023-06-02 10:15:36 +0900

    fixes to lbmon6 ci script

--- cicd/tcplbmon6/config.sh
+++ cicd/tcplbmon6/config.sh
@@ -82,4 +82,3 @@ $hexec llb1 ethtool -K ellb1l3ep3 gso off
 sleep 5
 $dexec llb1 loxicmd create lb 20.20.20.1 --tcp=2020:8080 --endpoints=31.31.31.1:1,32.32.32.1:1,33.33.33.1:1
 $dexec llb1 loxicmd create lb  2001::1 --tcp=2020:8080 --endpoints=4ffe::1:1,5ffe::1:1,6ffe::1:1 --monitor
-sleep 15
