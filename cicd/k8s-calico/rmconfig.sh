f08532cfbae87aedb99ee79ecbdf55eb9917c560
Nikhil Malik <nikhil.malik@netlox.io>
2023-06-09 16:09:25 +0900

    K8s cluster with calico and loxilb CICD added

---
+++ cicd/k8s-calico/rmconfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo ip route del 123.123.123.1 via 192.168.90.9
+vagrant destroy -f worker2
+vagrant destroy -f worker1
+vagrant destroy -f master
+vagrant destroy -f loxilb
+sudo rm loxilb-ip
