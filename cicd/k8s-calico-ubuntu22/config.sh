5953a49cba4e0967641d00b927ac834c310c4ea6
Nikhil Malik <nikhil.malik@netlox.io>
2023-06-13 11:16:26 +0900

    Checks added to make sure K8s cluster is up

--- cicd/k8s-calico-ubuntu22/config.sh
+++ cicd/k8s-calico-ubuntu22/config.sh
@@ -3,17 +3,37 @@ vagrant global-status  | grep -i virtualbox | cut -f 1 -d ' ' | xargs -L 1 vagra
 vagrant up
 sudo ip route add 123.123.123.1 via 192.168.90.9
 
+for((i=1; i<=60; i++))
+do
+    fin=1
+    pods=$(vagrant ssh master -c 'kubectl get pods -A' 2> /dev/null | grep -v "NAMESPACE")
+
+    while IFS= read -a pods; do
+        read -a pod <<< "$pods"
+        if [[ ${pod[3]} != *"Running"* ]]; then
+            echo "${pod[1]} is not UP yet"
+            fin=0
+        fi
+    done <<< "$pods"
+    if [ $fin == 1 ];
+    then
+        break;
+    fi
+    echo "Will try after 10s"
+    sleep 10
+done
+
 #Create default Service
-vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/tcp.yml'
-vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/udp.yml'
-vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/sctp.yml'
+vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/tcp.yml' 2> /dev/null
+vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/udp.yml' 2> /dev/null
+vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/sctp.yml' 2> /dev/null
 
 #Create onearm Service
-vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/tcp_onearm.yml'
-vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/udp_onearm.yml'
-vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/sctp_onearm.yml'
+vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/tcp_onearm.yml' 2> /dev/null
+vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/udp_onearm.yml' 2> /dev/null
+vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/sctp_onearm.yml' 2> /dev/null
 
 #Create fullnat Service
-vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/tcp_fullnat.yml'
-vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/udp_fullnat.yml'
-vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/sctp_fullnat.yml'
+vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/tcp_fullnat.yml' 2> /dev/null
+vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/udp_fullnat.yml' 2> /dev/null
+vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/sctp_fullnat.yml' 2> /dev/null
--- cicd/k8s-calico/config.sh
+++ cicd/k8s-calico/config.sh
@@ -3,6 +3,26 @@ vagrant global-status  | grep -i virtualbox | cut -f 1 -d ' ' | xargs -L 1 vagra
 vagrant up
 sudo ip route add 123.123.123.1 via 192.168.90.9
 
+for((i=1; i<=60; i++))
+do
+    fin=1
+    pods=$(vagrant ssh master -c 'kubectl get pods -A' 2> /dev/null | grep -v "NAMESPACE")
+
+    while IFS= read -a pods; do
+        read -a pod <<< "$pods"
+        if [[ ${pod[3]} != *"Running"* ]]; then
+            echo "${pod[1]} is not UP yet"
+            fin=0
+        fi
+    done <<< "$pods"
+    if [ $fin == 1 ];
+    then
+        break;
+    fi
+    echo "Will try after 10s"
+    sleep 10
+done
+
 #Create default Service
 vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/tcp.yml' 2> /dev/null
 vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/udp.yml' 2> /dev/null
