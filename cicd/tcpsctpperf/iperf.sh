f93c8d0dc9434fa355474798d7ff541c207b978a
Nikhil Malik <nikhil.malik@netlox.io>
2023-06-14 12:12:39 +0900

    iperf.sh added in perf case

---
+++ cicd/tcpsctpperf/iperf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+time=10
+count=$1
+
+iperf -c 20.20.20.1 -t $time -p 12865 -P $count > iperf.log &
+sleep $((time + 2))
+res=$(grep SUM iperf.log | tail -1| xargs | cut -d ' ' -f 6)
+unit=$(grep SUM iperf.log | tail -1| xargs | cut -d ' ' -f 7)
+echo -e "TCP throughput \t\t: $res $unit"
+rm -rf iperf.log
+
+sleep 2
+
+iperf3 -c 20.20.20.1 -t $time -p 13866 -P $count --logfile iperf.log --sctp &
+sleep $((time + 2))
+res=$(grep SUM iperf.log | tail -1| xargs | cut -d ' ' -f 6)
+unit=$(grep SUM iperf.log | tail -1| xargs | cut -d ' ' -f 7)
+echo -e "SCTP throughput \t: $res $unit"
+rm -rf iperf.log
