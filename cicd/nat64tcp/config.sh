13c35ee756baaab460cb89b987be0dbcb9054bb6
Nikhil Malik <nikhil.malik@netlox.io>
2023-06-23 01:23:47 -0400

    loxilb IP tested as service IP - added in tcplb and nat64 cicd

--- cicd/nat64tcp/config.sh
+++ cicd/nat64tcp/config.sh
@@ -81,4 +81,5 @@ $hexec llb1 ethtool --offload ellb1l3ep3 rx off tx off
 $hexec llb1 ethtool -K ellb1l3ep3 gso off
 
 $dexec llb1 loxicmd create lb 2001::1 --tcp=2020:8080 --endpoints=31.31.31.1:1,32.32.32.1:1,33.33.33.1:1
+$dexec llb1 loxicmd create lb 3ffe::2 --tcp=2020:8080 --endpoints=31.31.31.1:1,32.32.32.1:1,33.33.33.1:1
 sleep 10
