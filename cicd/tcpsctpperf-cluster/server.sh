e5e3d161285f8208bac8395855f18d18e38d01cb
Nikhil Malik <nikhil.malik@netlox.io>
2023-06-27 04:37:49 -0400

    updated server.sh

--- cicd/tcpsctpperf-cluster/server.sh
+++ cicd/tcpsctpperf-cluster/server.sh
@@ -2,4 +2,4 @@ export SERVER_ADDR=$(ip a |grep global | grep -v '10.0.2.15' | grep '192.168.80'
 export CLIENT_ADDR=$(cat /vagrant/server-ip)
 
 docker pull loxilbio/seastar-dev:latest
-docker run -u root --cap-add SYS_ADMIN   --restart unless-stopped --privileged -dit -v /dev/log:/dev/log --net=host --entrypoint /bin/bash --name loxilbio/seastar-dev:latest
+docker run -u root --cap-add SYS_ADMIN   --restart unless-stopped --privileged -dit -v /dev/log:/dev/log --net=host --entrypoint /bin/bash --name seastar loxilbio/seastar-dev:latest
