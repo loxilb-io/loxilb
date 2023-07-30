44f7a02f10b9594dc5c6461cea268d4844f062ff
TrekkieCoder <trekkie@netlox.io>
2023-07-31 00:11:09 +0900

    cicd for in-cluster lb with k8s and calico

--- cicd/k8s-calico-incluster/node_scripts/worker.sh
+++ cicd/k8s-calico-incluster/node_scripts/worker.sh
@@ -16,10 +16,3 @@ sudo chown 1000:1000 /home/vagrant/.kube/config
 NODENAME=$(hostname -s)
 kubectl label node $(hostname -s) node-role.kubernetes.io/worker=worker
 EOF
-
-#Install routes for pod to client (fullnat service) in nodes
-sudo ip route add 123.123.123.1 via 192.168.80.9
-
-#Install routes for pod to client (default service) in nodes
-sudo ip route add 192.168.90.0/24 via 192.168.80.9
-
