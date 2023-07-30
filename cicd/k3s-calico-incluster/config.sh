886375a0ff2693baf490e93b11bebe39678062a8
TrekkieCoder <trekkie@netlox.io>
2023-07-30 13:24:59 +0900

    cicd for incluster-lb with calico

---
+++ cicd/k3s-calico-incluster/config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+vagrant global-status  | grep -i virtualbox | cut -f 1 -d ' ' | xargs -L 1 vagrant destroy -f
+vagrant up
+sudo ip route add 123.123.123.1 via 192.168.90.10 || true
---
+++ cicd/k3s-calico-incluster/rmconfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo ip route del 123.123.123.1 via 192.168.90.10 || true
+vagrant destroy -f worker1
+vagrant destroy -f worker2
+vagrant destroy -f master1
+vagrant destroy -f master2
+vagrant destroy -f host
