1904979ba1b504de13ac9e80e6d77ce55b71f426
Trekkie Coder <trekkie@netlox.io>
2023-07-31 17:03:54 +0900

    cicd: k3s/flannel with in-cluster scripts updated

--- cicd/k3s-calico-incluster/validation.sh
+++ cicd/k3s-calico-incluster/validation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ../common.sh
-echo k3s-flannel-cluster
+echo k3s-calico-incluster
 
 if [ "$1" ]; then
   KUBECONFIG="$1"
@@ -9,48 +9,34 @@ fi
 # Set space as the delimiter
 IFS=' '
 
-sleep 45
+sleep 5
 extIP="123.123.123.1"
 echo $extIP
 
 echo "Service Info"
-vagrant ssh master -c 'sudo kubectl get svc'
-echo "LB Info"
-vagrant ssh loxilb -c 'sudo docker exec -i loxilb loxicmd get lb -o wide'
-echo "EP Info"
-vagrant ssh loxilb -c 'sudo docker exec -i loxilb loxicmd get ep -o wide'
+vagrant ssh master1 -c 'sudo kubectl get svc'
 
 print_debug_info() {
-  echo "llb1 route-info"
-  vagrant ssh loxilb -c 'ip route'
-  vagrant ssh master -c 'sudo kubectl get pods -A'
-  vagrant ssh master -c 'sudo kubectl get svc'
-  vagrant ssh master -c 'sudo kubectl get nodes'
+  echo "cluster-info"
+  vagrant ssh master1 -c 'sudo kubectl get pods -A'
+  vagrant ssh master1 -c 'sudo kubectl get svc'
+  vagrant ssh master1 -c 'sudo kubectl get nodes'
 }
 
 out=$(curl -s --connect-timeout 10 http://$extIP:55002) 
 if [[ ${out} == *"Welcome to nginx"* ]]; then
-  echo "k3s-flannel-cluster (kube-loxilb) tcp [OK]"
+  echo "k3s-calico-incluster (kube-loxilb) tcp [OK]"
 else
-  echo "k3s-flannel-cluster (kube-loxilb) tcp [FAILED]"
+  echo "k3s-calico-incluster (kube-loxilb) tcp [FAILED]"
   print_debug_info
   exit 1
 fi
 
 out=$(timeout 10 ../common/udp_client $extIP 55003)
 if [[ ${out} == *"Client"* ]]; then
-  echo "k3s-flannel-cluster (kube-loxilb) udp [OK]"
+  echo "k3s-calico-incluster (kube-loxilb) udp [OK]"
 else
-  echo "k3s-flannel-cluster (kube-loxilb) udp [FAILED]"
-  print_debug_info
-  exit 1
-fi
-
-out=$(timeout 10 ../common/sctp_client 192.168.90.1 41291 $extIP 55004)
-if [[ ${out} == *"server1"* ]]; then
-  echo "k3s-flannel-cluster (kube-loxilb) sctp [OK]"
-else
-  echo "k3s-flannel-cluster (kube-loxilb) sctp [FAILED]"
+  echo "k3s-calico-incluster (kube-loxilb) udp [FAILED]"
   print_debug_info
   exit 1
 fi
