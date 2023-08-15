2945e85e98e25a841a285c410b2167221ea1e559
Trekkie Coder <trekkie@netlox.io>
2023-08-15 11:52:58 +0900

    updated cicd for sctp-multihoming

--- cicd/k3s-sctpmh-2/keepalived_config2/notify.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-declare -A vip
-vip["default"]="11.11.11.11"
-echo $1 $2 is in $3 state vip ${vip[$2]}> /etc/shared/keepalive.state
-curl -X 'POST' 'http://0.0.0.0:11111/netlox/v1/config/cistate' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ "instance": "'$2'", "state" : "'$3'", "vip" : "'${vip[$2]}'" }'
