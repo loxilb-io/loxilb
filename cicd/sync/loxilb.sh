5c8d504809b7771b547249d22baf927a9b2799ac
Nikhil Malik <nikhil.malik@netlox.io>
2023-08-16 17:09:16 +0900

    sync test case initial scripts

---
+++ cicd/sync/loxilb.sh
@@ -0,0 +1,17 @@
+export LOXILB_IP=$(ip a |grep global | grep -v '10.0.2.15' | grep -v '192.168.80' | awk '{print $2}' | cut -f1 -d '/')
+
+apt-get update
+apt-get install -y software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+apt-get install -y docker-ce
+docker run -u root --cap-add SYS_ADMIN   --restart unless-stopped --privileged --entrypoint /bin/bash -dit -v /dev/log:/dev/log --net=host --name loxilb ghcr.io/loxilb-io/loxilb:latest
+
+docker exec -it loxilb apt update
+docker exec -it loxilb apt -y install clang-10 llvm libelf-dev gcc-multilib libpcap-dev linux-tools-$(uname -r) elfutils dwarves git libbsd-dev bridge-utils unzip build-essential bison flex iperf iproute2 nodejs socat ethtool
+docker exec -it loxilb git clone https://github.com/loxilb-io/loxilb.git --recurse-submodules
+
+echo alias loxicmd=\"sudo docker exec -it loxilb loxicmd\" >> ~/.bashrc
+echo alias loxilb=\"sudo docker exec -it loxilb \" >> ~/.bashrc
+
+echo $LOXILB_IP > /vagrant/loxilb-$(hostname)
---
+++ cicd/sync/validation.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+source ../common.sh
+echo k3s-cilium-cluster
+
+if [ "$1" ]; then
+  KUBECONFIG="$1"
+fi
+
+# Set space as the delimiter
+IFS=' '
+
+sleep 45
+extIP="123.123.123.1"
+echo $extIP
+
+echo "Service Info"
+vagrant ssh master -c 'sudo kubectl get svc'
+echo "LB Info"
+vagrant ssh loxilb -c 'sudo docker exec -i loxilb loxicmd get lb -o wide'
+echo "EP Info"
+vagrant ssh loxilb -c 'sudo docker exec -i loxilb loxicmd get ep -o wide'
+
+print_debug_info() {
+  echo "llb1 route-info"
+  vagrant ssh loxilb -c 'ip route'
+  vagrant ssh master -c 'sudo kubectl get pods -A'
+  vagrant ssh master -c 'sudo kubectl get svc'
+  vagrant ssh master -c 'sudo kubectl get nodes'
+}
+
+out=$(curl -s --connect-timeout 10 http://$extIP:55002) 
+if [[ ${out} == *"Welcome to nginx"* ]]; then
+  echo "k3s-cilium-cluster (kube-loxilb) tcp [OK]"
+else
+  echo "k3s-cilium-cluster (kube-loxilb) tcp [FAILED]"
+  print_debug_info
+  exit 1
+fi
+
+out=$(timeout 10 ../common/udp_client $extIP 55003)
+if [[ ${out} == *"Client"* ]]; then
+  echo "k3s-cilium-cluster (kube-loxilb) udp [OK]"
+else
+  echo "k3s-cilium-cluster (kube-loxilb) udp [FAILED]"
+  print_debug_info
+  exit 1
+fi
+
+exit
