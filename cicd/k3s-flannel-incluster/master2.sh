1fb24c0d9ecfb7806dab43126fd7813121e93855
Trekkie Coder <trekkie@netlox.io>
2023-10-15 15:22:44 +0900

    cicd:fixes to incluster scenario

--- cicd/k3s-flannel-incluster/master2.sh
+++ cicd/k3s-flannel-incluster/master2.sh
@@ -5,6 +5,6 @@ export NODE_TOKEN=$(cat /vagrant/node-token)
 
 #curl -fL https://get.k3s.io | K3S_TOKEN=${NODE_TOKEN} sh -s - server --server https://192.168.80.10:6443 --disable traefik --disable servicelb --node-ip=192.168.80.11 external-hostname=192.168.80.11 --node-external-ip=192.168.80.11 --disable-cloud-controller -t ${NODE_TOKEN}
 curl -fL https://get.k3s.io | K3S_TOKEN=${NODE_TOKEN} sh -s - server --server https://192.168.80.10:6443 --disable traefik --disable servicelb --node-ip=192.168.80.11 external-hostname=192.168.80.11 --node-external-ip=192.168.80.11 -t ${NODE_TOKEN}
-curl -sfL https://github.com/loxilb-io/loxilb-ebpf/raw/main/kprobe/install.sh | sh -
+#curl -sfL https://github.com/loxilb-io/loxilb-ebpf/raw/main/kprobe/install.sh | sh -
 
 /vagrant/wait_ready.sh
