db80cbb9b629399a9b516da0923f63262241eec0
Nikhil Malik <nikhil.malik@netlox.io>
2023-12-12 16:01:51 +0900

    K8s HA CICD case added

---
+++ cicd/k8s-calico-ipvs3-ha/node_scripts/host.sh
@@ -0,0 +1,14 @@
+# Install Bird to work with k3s
+sudo apt-get -y install bird2 lksctp-tools iperf
+
+sudo ip addr add 30.30.30.1/32 dev lo
+
+sleep 5
+
+sudo cp -f /vagrant/bird_config/bird.conf /etc/bird/bird.conf
+if [ ! -f  /var/log/bird.log ]; then
+  sudo touch /var/log/bird.log
+fi
+sudo chown bird:bird /var/log/bird.log
+sudo service bird restart
+echo "Host is up"
