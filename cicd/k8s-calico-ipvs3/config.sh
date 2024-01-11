ab95389de415c9caf0aef69f60c871d588a59fe8
Nikhil Malik <nikhil.malik@netlox.io>
2024-01-11 11:04:39 +0900

    k8s-calico-ipvs3 CICD test scripts updated

--- cicd/k8s-calico-ipvs3/config.sh
+++ cicd/k8s-calico-ipvs3/config.sh
@@ -36,3 +36,30 @@ sudo sysctl net.ipv4.conf.vboxnet1.arp_accept=1
 vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/tcp_onearm.yml' 2> /dev/null
 vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/udp_onearm.yml' 2> /dev/null
 vagrant ssh master -c 'kubectl apply -f /vagrant/yaml/sctp_onearm.yml' 2> /dev/null
+
+for((i=1; i<=60; i++))
+do
+    fin=1
+    pods=$(vagrant ssh master -c 'kubectl get pods -A' 2> /dev/null | grep -v "NAMESPACE")
+
+    while IFS= read -a pods; do
+        read -a pod <<< "$pods"
+        if [[ ${pod[3]} != *"Running"* ]]; then
+            echo "${pod[1]} is not UP yet"
+            fin=0
+        fi
+    done <<< "$pods"
+    if [ $fin == 1 ];
+    then
+        break;
+    fi
+    echo "Will try after 10s"
+    sleep 10
+done
+
+if [[ $fin == 0 ]]; then
+echo "Cluster is not ready"
+exit 1
+fi
+echo "Cluster is ready"
+
