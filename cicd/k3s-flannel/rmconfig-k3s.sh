517da7d68d26157c792a916d2286557fec67180c
Trekkie Coder <trekkie@netlox.io>
2024-02-23 14:51:06 +0900

    cicd: updated k3s-flannel scenario

---
+++ cicd/k3s-flannel/rmconfig-k3s.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo "#########################################"
+echo "Removing testbed"
+echo "#########################################"
+
+if [ "$1" ]; then
+  KUBECONFIG="$1"
+fi
+
+source ../common.sh
+
+#sudo ip route del 11.11.11.11/32 via 14.14.14.1
+#sudo ip route del 123.123.123.1/32 via 14.14.14.1
+
+sudo kubectl $KUBECONFIG delete -f nginx-svc-lb1.yml >> /dev/null 2>&1
+sudo kubectl $KUBECONFIG delete -f nginx-svc-lb.yml >> /dev/null 2>&1
+sudo kubectl $KUBECONFIG delete -f nginx.yml >> /dev/null 2>&1
+sudo kubectl $KUBECONFIG delete -f udp-svc-lb.yml >> /dev/null 2>&1
+sudo kubectl $KUBECONFIG delete -f kube-loxilb.yml >> /dev/null 2>&1
+#sudo kubectl $KUBECONFIG delete -f https://github.com/loxilb-io/loxi-ccm/raw/master/manifests/loxi-ccm-k3s.yaml >> /dev/null 2>&1
+
+disconnect_docker_hosts user r1
+disconnect_docker_hosts r1 llb1
+disconnect_docker_hosts r1 llb2
+disconnect_docker_hosts llb1 r2
+disconnect_docker_hosts llb2 r2
+disconnect_docker_hosts r2 ep1
+disconnect_docker_hosts r2 ep2
+disconnect_docker_hosts r2 ep3
+
+delete_docker_host ka_llb1
+delete_docker_host ka_llb2
+delete_docker_host llb1
+delete_docker_host llb2
+delete_docker_host user
+delete_docker_host r1
+delete_docker_host r2
+delete_docker_host ep1
+delete_docker_host ep2
+delete_docker_host ep3
+sudo brctl delif k3sbr esysllb2
+sudo brctl delif k3sbr esysllb1
+sudo ip link set k3sbr down
+sudo brctl delbr k3sbr
+sudo ip link del esysllb1
+sudo ip link del esysllb2
+
+# If k3s setup exists, remove it
+if [[ -f "/usr/local/bin/k3s-uninstall.sh" ]]; then
+  /usr/local/bin/k3s-uninstall.sh
+fi
+
+sudo apt-get remove bird2 --yes
+
+echo "#########################################"
+echo "Removed testbed"
+echo "#########################################"
---
+++ cicd/k3s-flannel/validation-k3s.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+source ../common.sh
+echo cluster-k3s
+
+if [ "$1" ]; then
+  KUBECONFIG="$1"
+fi
+
+# Set space as the delimiter
+IFS=' '
+
+for((i=0; i<120; i++))
+do
+  extLB=$(sudo kubectl $KUBECONFIG get svc | grep "nginx-lb1")
+  read -a strarr <<< "$extLB"
+  len=${#strarr[*]}
+  if [[ $((len)) -lt 6 ]]; then
+    echo "Can't find nginx-lb service"
+    sleep 1
+    continue
+  fi 
+  if [[ ${strarr[3]} != *"none"* ]]; then
+    extIP="$(cut -d'-' -f2 <<<${strarr[3]})"
+    break
+  fi
+  echo "No external LB allocated"
+  sleep 1
+done
+
+## Any routing updates  ??
+sleep 60
+
+echo "ExternalIP $extIP"
+
+print_debug_info() {
+  ## Dump some debug info
+  echo "**** k3s svc info ****"
+  sudo kubectl $KUBECONFIG get svc
+  echo "**** k3s pods info ****"
+  sudo kubectl $KUBECONFIG get pods -A
+
+  echo "**** llb1 lb-info ****"
+  $dexec llb1 loxicmd get lb -o wide
+  echo "**** loxilb ep-info ****"
+  $dexec llb1 loxicmd get ep -o wide
+  echo "**** llb1 route-info ****"
+  $dexec llb1 ip route
+  echo "**** llb1 bfd-info ****"
+  $dexec llb1 loxicmd get ha
+
+  echo "**** llb2 lb-info ****"
+  $dexec llb2 loxicmd get lb -o wide
+  echo "**** loxilb ep-info ****"
+  $dexec llb1 loxicmd get ep -o wide
+  echo "**** llb2 route-info ****"
+  $dexec llb2 ip route
+  echo "**** llb2 bfd-info ****"
+  $dexec llb2 loxicmd get ha
+
+  echo "**** r1 route-info ****"
+  $dexec r1 ip route
+
+  echo "**** sys route-info ****"
+  ip route
+}
+
+code=0
+echo "********************"
+print_debug_info
+echo "********************"
+
+out=$($hexec user curl -s --connect-timeout 10 http://$extIP:55002) 
+if [[ ${out} == *"Welcome to nginx"* ]]; then
+  echo "cluster-k3s (kube-loxilb) tcp [OK]"
+else
+  echo "cluster-k3s (kube-loxilb) tcp [FAILED]"
+  code=1
+fi
+
+for((i=0; i<120; i++))
+do
+  extLB=$(sudo kubectl $KUBECONFIG get svc | grep "udp-lb1")
+  read -a strarr <<< "$extLB"
+  len=${#strarr[*]}
+  if [[ $((len)) -lt 6 ]]; then
+    echo "Can't find udp-lb service"
+    sleep 1
+    continue
+  fi
+  if [[ ${strarr[3]} != *"none"* ]]; then
+    extIP="$(cut -d'-' -f2 <<<${strarr[3]})"
+    break
+  fi
+  echo "No external LB allocated"
+  sleep 1
+done
+
+out=$($hexec user timeout 30 ../common/udp_client $extIP 55003)
+if [[ ${out} == *"Client"* ]]; then
+  echo "cluster-k3s (kube-loxilb) udp [OK]"
+else
+  echo "cluster-k3s (kube-loxilb) udp [FAILED]"
+  code=1
+fi
+
+if [[ $code -eq 1 ]]; then
+  print_debug_info
+  echo "cluster-k3s failed"
+  touch /vagrant/error
+  exit 1
+fi
+
+exit
