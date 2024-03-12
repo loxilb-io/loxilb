5e4a7f260367b7bb5973726de8e42688d188488e
Nikhil Malik <nikhil.malik@netlox.io>
2024-03-12 13:40:39 +0900

    docker-k3s-cilium validation script updated

--- cicd/docker-k3s-cilium/validation.sh
+++ cicd/docker-k3s-cilium/validation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ../common.sh
-echo docker-k3s-lb
+echo docker-k3s-cilium
 
 if [ "$1" ]; then
   KUBECONFIG="$1"
@@ -9,41 +9,61 @@ fi
 # Set space as the delimiter
 IFS=' '
 
-sleep 30
-extIP="192.168.163.247"
+for((i=0; i<120; i++))
+do
+  extLB=$(vagrant ssh llb1 -c 'sudo kubectl get svc' 2> /dev/null | grep tcp-lb"")
+  read -a strarr <<< "$extLB"
+  len=${#strarr[*]}
+  if [[ $((len)) -lt 6 ]]; then
+    echo "Can't find tcp-lb service"
+    sleep 1
+    continue
+  fi 
+  if [[ ${strarr[3]} != *"none"* ]]; then
+    extIP="$(cut -d'-' -f2 <<<${strarr[3]})"
+    break
+  fi
+  echo "No external LB allocated"
+  sleep 1
+done
+
+## Any routing updates  ??
+#sleep 30
+echo Service IP : $extIP
 echo $extIP
 
-echo "Service Info"
-vagrant ssh llb1 -c 'sudo kubectl get svc'
-echo "LB Info"
-vagrant ssh llb1 -c 'sudo docker exec -i loxilb loxicmd get lb -o wide'
-echo "EP Info"
-vagrant ssh llb1 -c 'sudo docker exec -i loxilb loxicmd get ep -o wide'
+echo -e "\nEnd Points List"
+echo "******************************************************************************"
+vagrant ssh llb1 -c 'sudo kubectl get endpoints -A' 2> /dev/null
+echo "******************************************************************************"
+echo -e "\nSVC List"
+echo "******************************************************************************"
+vagrant ssh llb1 -c 'sudo kubectl get svc' 2> /dev/null
+echo "******************************************************************************"
+echo -e "\nPod List"
+echo "******************************************************************************"
+vagrant ssh llb1 -c 'sudo kubectl get pods -A' 2> /dev/null
+echo "******************************************************************************"
+echo -e "\nLB List"
+echo "******************************************************************************"
+vagrant ssh llb1 -c 'sudo sudo docker exec -it loxilb loxicmd get lb -o wide' 2> /dev/null
+echo "******************************************************************************"
+echo -e "\nEP List"
+echo "******************************************************************************"
+vagrant ssh llb1 -c 'sudo docker exec -it loxilb loxicmd get ep -o wide' 2> /dev/null
+echo "******************************************************************************"
 
-print_debug_info() {
-  echo "llb1 route-info"
-  vagrant ssh llb1 -c 'ip route'
-  vagrant ssh llb1 -c 'sudo kubectl get pods -A'
-  vagrant ssh llb1 -c 'sudo kubectl get svc'
-  vagrant ssh llb1 -c 'sudo kubectl get nodes'
-}
+echo -e "\nTEST RESULTS"
+echo "******************************************************************************"
 
-sctp_darn -H 192.168.163.1 -h 192.168.163.247 -p 55003 -s < input > output
-sleep 5
-exp="New connection, peer addresses
-192.168.163.247:55003"
-
-res=`cat output | grep -A 1 "New connection, peer addresses"`
+echo -e "Command: curl --connect-time 10 http://192.168.163.247:56002'\n"
+res=`curl -s --connect-time 10 http://192.168.163.247:56002`
 echo "Result"
 echo $res
-echo "Expected"
-echo $exp
-sudo rm -rf output
-if [[ "$res" == "$exp" ]]; then
-    echo $res
-    echo "docker-k3s-lb SCTP service sctp-lb (loxilb) [OK]"
+if [[ "$res" == *"Welcome to nginx"* ]]; then
+    echo -e "\n\ndocker-k3s-cilium TCP service (loxilb) [OK]"
 else
-    echo "docker-k3s-lb SCTP service sctp-lb (loxilb) [NOK]"
-    print_debug_info 
+    echo -e "\n\ndocker-k3s-cilium TCP service (loxilb) [NOK]"
     exit 1
 fi
+
