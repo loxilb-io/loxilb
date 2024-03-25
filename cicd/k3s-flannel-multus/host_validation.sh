64e1c582ef9de3a486033cdd009926e0cf5a5fda
Nikhil Malik <nikhil.malik@netlox.io>
2024-03-25 16:29:33 +0900

    sctp mh case added

--- cicd/k3s-flannel-multus/host_validation.sh
+++ cicd/k3s-flannel-multus/host_validation.sh
@@ -2,6 +2,7 @@
 extIP=$(cat /vagrant/extIP)
 
 tcp_port=55002
+sctp_port=55002
 
 code=0
 echo Service IP: $extIP
@@ -15,6 +16,28 @@ if [[ ${out} == *"Welcome to nginx"* ]]; then
 else
   echo -e "K3s-multus TCP\t($mode)\t[FAILED]"
   code=1
+fi
+done
+
+for((i=0;i<20;i++))
+do
+stdbuf -oL sctp_darn -H 0.0.0.0 -h $extIP -p $sctp_port -s < /vagrant/input > output
+#sleep 1
+exp="New connection, peer addresses
+4.0.5.2:55002
+4.0.3.1:55002
+4.0.4.1:55002"
+
+res=`cat output | grep -A 3 "New connection, peer addresses"`
+sudo rm -rf output
+if [[ "$res" == "$exp" ]]; then
+    #echo $res
+    echo "K3s-multus SCTP Multihoming service  [OK]"
+else
+    echo "K3s-multus SCTP Multihoming service  [NOK]"
+    echo "Expected : $exp"
+    echo "Received : $res"
+    exit 1
 fi
 done
 exit $code
