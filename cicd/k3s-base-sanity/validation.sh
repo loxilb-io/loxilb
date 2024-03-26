5fa620977dd0c27dcc1eb3ddf76c61730d5bf14d
Nikhil Malik <nikhil.malik@netlox.io>
2024-03-26 15:32:05 +0900

    sctp test added in k3s-base-sanity

--- cicd/k3s-base-sanity/validation.sh
+++ cicd/k3s-base-sanity/validation.sh
@@ -71,9 +71,9 @@ echo "********************"
 
 out=$($hexec user curl -s --connect-timeout 10 http://$extIP:55002) 
 if [[ ${out} == *"Welcome to nginx"* ]]; then
-  echo "cluster-k3s (kube-loxilb) tcp [OK]"
+  echo "cluster-k3s (kube-loxilb) tcp  [OK]"
 else
-  echo "cluster-k3s (kube-loxilb) tcp [FAILED]"
+  echo "cluster-k3s (kube-loxilb) tcp  [FAILED]"
   code=1
 fi
 
@@ -97,17 +97,54 @@ done
 
 out=$($hexec user timeout 30 ../common/udp_client $extIP 55003)
 if [[ ${out} == *"Client"* ]]; then
-  echo "cluster-k3s (kube-loxilb) udp [OK]"
+  echo "cluster-k3s (kube-loxilb) udp  [OK]"
 else
-  echo "cluster-k3s (kube-loxilb) udp [FAILED]"
+  echo "cluster-k3s (kube-loxilb) udp  [FAILED]"
   code=1
 fi
 
+for((i=0; i<120; i++))
+do
+  extLB=$(sudo kubectl $KUBECONFIG get svc | grep "sctp-lb")
+  read -a strarr <<< "$extLB"
+  len=${#strarr[*]}
+  if [[ $((len)) -lt 6 ]]; then
+    echo "Can't find sctp-lb service"
+    sleep 1
+    continue
+  fi
+  if [[ ${strarr[3]} != *"none"* ]]; then
+    extIP="$(cut -d'-' -f2 <<<${strarr[3]})"
+    break
+  fi
+  echo "No external LB allocated"
+  sleep 1
+done
+
+$hexec user timeout 5 stdbuf -oL sctp_darn -H 0.0.0.0 -h $extIP -p 55003 -s < input > output
+sleep 5
+exp="New connection, peer addresses
+$extIP:55003"
+
+res=`cat output | grep -A 1 "New connection, peer addresses"`
+#echo "Result"
+#echo $res
+#echo "Expected"
+#echo $exp
+sudo rm -rf output
+if [[ "$res" == "$exp" ]]; then
+    #echo $res
+    echo "cluster-k3s (kube-loxilb) sctp [OK]"
+else
+    echo "cluster-k3s (kube-loxilb) sctp [NOK]"
+    print_debug_info 
+    code=1
+fi
 if [[ $code -eq 1 ]]; then
   print_debug_info
   echo "cluster-k3s failed"
-  touch /vagrant/error
   exit 1
 fi
+
 
 exit
