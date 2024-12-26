4c6a60ceb403ad550dfabe959b230188782a2f15
Trekkie Coder <trekkie@netlox.io>
2024-12-26 23:18:55 +0900

    gh-877 Initial support for egress : updated cicd

--- cicd/egresslb/validation.sh
+++ cicd/egresslb/validation.sh
@@ -5,6 +5,16 @@ echo SCENARIO-egrlb
 sleep 10 
 code=0
 
+check_egr_conn() {
+    IP="$1"
+    if $hexec h1 curl -s --head --connect-timeout 5 http://$IP | grep -i "nginx" > /dev/null; then
+        echo "Connectivity to http://$IP is OK."
+    else
+        echo "Failed to connect to https://$IP"
+        code=1
+    fi
+}
+
 check_ping() {
     IP="$1"
     if $hexec h1 ping -c 5 -W 10 "$IP" &> /dev/null; then
@@ -17,8 +27,10 @@ check_ping() {
 
 echo "Checking egress before HA"
 
-IP_ADDRESS="8.8.8.8"
-check_ping $IP_ADDRESS
+PING_IP_ADDRESS="8.8.8.8"
+CURL_IP_ADDRESS="208.67.222.222"
+#check_ping $PING_IP_ADDRESS
+check_egr_conn $CURL_IP_ADDRESS
 
 $hexec llb2 curl -X 'POST' \
   'http:/127.0.0.1:11111/netlox/v1/config/cistate' \
@@ -43,7 +55,8 @@ $hexec llb1 curl -X 'POST' \
 sleep 10
 echo "Checking egress after HA"
 
-check_ping $IP_ADDRESS
+#check_ping $IP_ADDRESS
+check_egr_conn $CURL_IP_ADDRESS
 
 if [[ $code != 0 ]];then
   echo "SCENARIO-egrlb FAILED"
